7ea402882b077d9cd8385b4d9250d94ca4beb98e
rajapragadesh <rajapragadesh1994@gmail.com>
2021-09-13 17:40:45 +0530

    require statement change

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const changeLogDetails = require('./routes/changeLog');
+const feedbackDetails = require('./routes/feedback');
+const changeLog = require('./models/changeLog');
 
 mongoose.connect(keys.mongoURI , {
     useNewUrlParser: true,
@@ -11,22 +13,45 @@ mongoose.connect(keys.mongoURI , {
 
 const app = express();
 
-const changeLogDetails = require('./routes/changeLog');
-const feedbackDetails = require('./routes/feedback');
-
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
 app.use('/changelog',changeLogDetails);
 app.use('/feedback', feedbackDetails);
 
+app.get('/changes/uniqueTags', (req, res) => {
+
+    console.log("###", "uniqueTags");
+
+    changeLog.aggregate([
+        { $unwind : "$category"},
+        { $group : {_id :"$category"}} 
+    ]).exec()
+    .then((tags) => {
+                console.log(tags);
+                    res.status(200).json({
+                        tags : tags,
+                        noOftags : tags.length,
+                    });
+    }).catch((err) => {
+        res.status(500).json({
+            message : "tags not found"
+        });
+    });
+
+});
+
+
+
+
 app.use((req,res, next) => {
     const error  = new Error('not found');
     error.status = 404;
-    next();
+    next(error);
 });
 
 app.use((error, req, res, next) => {
+    console.log("$$$", error);
     res.status(error.status || 500);
     res.json({
         error : {
