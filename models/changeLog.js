9d78edfc99179502841514db3bb82035fbea5aad
rajapragadesh <rajapragadesh1994@gmail.com>
2021-10-28 17:51:10 +0530

    showing changeLog with customized props

--- models/changeLog.js
+++ models/changeLog.js
@@ -8,7 +8,8 @@ const changeLogSchema = mongoose.Schema(
         body : {type : String, required : true, default : null},
         like : {type : Number, default : 0 },
         dislike : {type : Number , default : 0 },
-        accId : { type : String, default : null}
+        accId : { type : String, default : null},
+        __customers : { type : [String], default : null}
         // __user : {type : Schema.Types.ObjectId , ref : 'User'}
     },
     {
