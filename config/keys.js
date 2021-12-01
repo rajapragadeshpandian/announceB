4f18d5b93c7d87b7c347be4c8270363553656dce
rajapragadesh <rajapragadesh1994@gmail.com>
2021-12-01 18:12:59 +0530

    passport

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,12 @@
 module.exports = {
-    //GoogleClientID : '169500766019-1q7183k7549etjip7kjh4dsdq9ac8s92.apps.googleusercontent.com',
-    //GoogleClientSecret : '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',
-    //used old client id and secret below 
-    //above are the new ones
-    GoogleClientID : '6271239961-s6mn9stv0n8nnp50oade523lkaht4su5.apps.googleusercontent.com',
-    GoogleClientSecret : '3l2koXMkudOLCA_evqH43e_N',
+    GoogleClientID : '169500766019-1q7183k7549etjip7kjh4dsdq9ac8s92.apps.googleusercontent.com',
+    GoogleClientSecret : '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',
     mongoURI : 'mongodb+srv://pragadesh:K6pD0Kr1RLBJ3j54@cluster0.hj629.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
-
+   //used old client id and secret below 
+    //above are the new ones
+    //GoogleClientID : '6271239961-s6mn9stv0n8nnp50oade523lkaht4su5.apps.googleusercontent.com',
+    //GoogleClientSecret : '3l2koXMkudOLCA_evqH43e_N',
 // GoogleClientID
 // 169500766019-1q7183k7549etjip7kjh4dsdq9ac8s92.apps.googleusercontent.com
 // GoogleClientSecret
