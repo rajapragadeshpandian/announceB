6099252e9504b9fe377e4372a615f35953253219
rajapragadesh <rajapragadesh1994@gmail.com>
2022-01-05 22:29:16 +0530

    focus changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
-
+import Hook from './Hook';
 
 function App() {
+  const text = "React Hooks";
   return (
     <div className="App">
       <h1>AppCoach</h1>
+    <Hook text={text}/>
     </div>
   );
 }
